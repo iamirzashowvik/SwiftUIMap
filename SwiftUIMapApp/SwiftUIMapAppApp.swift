8e6e0ca3cd27eab43fcf187bfdeeadd0f055ed38
Mirza Showvik <showvikmirza@gmail.com>
2023-10-28 23:27:35 +0600

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Mirza Showvik on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
